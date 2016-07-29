7b7b8c2748eef24c9c9f92bfe7c7817f33a09b76
Michael Verkooijen <michaelverkooijen@gmail.com>
2016-07-29 21:31:45 +0200

    Delete button

--- specialpage.js
+++ specialpage.js
@@ -5,13 +5,12 @@
 * @description: Creates a flat feed for discussions module on Special:DiscussionsFeed. Includes moderation tools.
 */
 
-function updateFeed(content) {
+function updateFeed(content, isMod) {
     var rcLimit = 25;
     console.log("sending xhr");
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if(request.readyState == 4 && request.status == 200) {
-            console.log(request.responseText);
             var arr = JSON.parse(request.responseText);
             content.innerHTML = '';
             for (var i = 0; i < rcLimit; i++) {
@@ -23,53 +22,66 @@ function updateFeed(content) {
                 var isReported = arr["_embedded"]["doc:posts"][i].isReported;
                 var dt = new Date(epoch * 1000);
 
+                //Create HTML:
+                var par = document.createElement("P");
+                par.className = "df-entry";
+
                 //Create HTML for date:
                 var spanDate = document.createElement("SPAN");
                 spanDate.className = "df-date";
                 var spanDateText = document.createTextNode(dt + ": ");
                 spanDate.appendChild(spanDateText);
+                par.appendChild(spanDate);
 
                 //Create HTML for message body:
                 var aMessage = document.createElement("A");
                 aMessage.className = "df-content";
-                aMessage.href = "https://elderscrolls.wikia.com/d/p/" + threadID + "/r/" + postID; //TODO: dynamic wiki name
+                aMessage.href = "https://" + wgDBname + ".wikia.com/d/p/" + threadID + "/r/" + postID;
                 aMessage.target = "_blank";
                 var aMessageText = document.createTextNode(text);
                 aMessage.appendChild(aMessageText);
+                par.appendChild(aMessage);
 
                 //Create HTML for user:
                 var spanUser = document.createElement("SPAN");
                 spanUser.className = "df-user";
                 var spanUserText = document.createTextNode(" — " + user);
                 spanUser.appendChild(spanUserText);
-
-                var par = document.createElement("P");
-                par.className = "df-entry";
-                par.appendChild(spanDate);
-                par.appendChild(aMessage);
                 par.appendChild(spanUser);
+
+                //Create Delete button for moderators:
+                if (isMod) {
+                    var btnDelete = document.createElement("button");
+                    btnDelete.type = "button";
+                    btnDelete.textContent = "Delete";
+                    btnDelete.addEventListener("click", deletePost(postID));
+                    par.appendChild(btnDelete);
+                }
+
                 content.appendChild(par);
             }
         }
     };
-    request.open("GET", "https://services.wikia.com/discussion/1706/posts?limit=" + rcLimit + "&page=0&responseGroup=small&reported=false&viewableOnly=true", true);
+    request.open("GET", "https://services.wikia.com/discussion/" + wgCityId + "/posts?limit=" + rcLimit + "&page=0&responseGroup=small&reported=false&viewableOnly=true", true);
     request.setRequestHeader('Accept', 'application/hal+json');
     request.send();
 }
 
 function deletePost(postID) {
+    console.log("Deleting post: " + postID);
     var request = new XMLHttpRequest();
-    request.open("PUT", "https://services.wikia.com/discussion/1706/posts/" + postID + "/delete", true);
+    request.open("PUT", "https://services.wikia.com/discussion/" + wgCityId + "/posts/" + postID + "/delete", true);
     request.send();
 }
 
 function createDiscussionsFeed() {
     if(wgPageName == 'Special:DiscussionsFeed') {
+        document.title = 'Discussions Feed - ' + wgSiteName;
         var canBlock = Boolean(wgUserGroups.indexOf('sysop') > -1 || wgUserGroups.indexOf('staff') > -1 || wgUserGroups.indexOf('helper') > -1 || wgUserGroups.indexOf('vstf') > -1);
         var isMod = Boolean(canBlock || wgUserGroups.indexOf('threadmoderator') > -1);
         var content = document.getElementById("mw-content-text");
         content.innerHTML = 'Loading feed...<div id="ajaxloader"></div>';
-        updateFeed(content);
+        updateFeed(content, isMod);
     }
 }
 
