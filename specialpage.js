e22744cf7e7e007cdf296755b5a7b937ef11ba03
Michael Verkooijen <michaelverkooijen@gmail.com>
2016-10-17 14:49:48 +0200

    v1.0.3 padded timestamp

--- specialpage.js
+++ specialpage.js
@@ -1,85 +1,98 @@
 /**
 * @author: Flightmare (http://elderscrolls.wikia.com/wiki/User:Flightmare)
-* @version: 1.0.2
+* @version: 1.0.3
 * @license: CC-BY-SA 3.0
 * @description: Creates a flat feed for discussions module on Special:DiscussionsFeed. Includes moderation tools.
 */
+
+function strPad(n) {
+    return String("00" + n).slice(-2);
+}
 
 function updateFeed(content, isMod, canBlock) {
     var rcLimit = 50;
     var request = new XMLHttpRequest();
     request.timeout = 30000; // 30 seconds
     request.ontimeout = function() {
-        content.innerHTML = "Unable to load discussions: The service is down or is not enabled for this domain."
-    }
+        content.innerHTML = "Unable to load discussions: The service is down or is not enabled for this domain.";
+    };
     request.onreadystatechange = function() {
-        if(request.readyState == 4 && request.status == 200) {
-            var arr = JSON.parse(request.responseText);
-            content.innerHTML = '';
-            for (var i = 0; i < rcLimit; i++) {
-                var text = arr["_embedded"]["doc:posts"][i].rawContent;
-                var user = arr["_embedded"]["doc:posts"][i]["createdBy"].name;
-                var userId = arr["_embedded"]["doc:posts"][i]["createdBy"].id;
-                var epoch = arr["_embedded"]["doc:posts"][i]["creationDate"].epochSecond;
-                var postID = arr["_embedded"]["doc:posts"][i].id;
-                var threadID = arr["_embedded"]["doc:posts"][i].threadId;
-                var isReported = Boolean(arr["_embedded"]["doc:posts"][i].isReported);
-                var forumName = arr["_embedded"]["doc:posts"][i].forumName;
-                var dt = new Date(epoch * 1000);
-                var formattedDate = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds()
+        if(request.readyState == 4) {
+            switch(request.status) {
+                case 200:
+                    var arr = JSON.parse(request.responseText);
+                    content.innerHTML = '';
+                    for (var i = 0; i < rcLimit; i++) {
+                        var text = arr["_embedded"]["doc:posts"][i].rawContent;
+                        var user = arr["_embedded"]["doc:posts"][i]["createdBy"].name;
+                        var userId = arr["_embedded"]["doc:posts"][i]["createdBy"].id;
+                        var epoch = arr["_embedded"]["doc:posts"][i]["creationDate"].epochSecond;
+                        var postID = arr["_embedded"]["doc:posts"][i].id;
+                        var threadID = arr["_embedded"]["doc:posts"][i].threadId;
+                        var isReported = Boolean(arr["_embedded"]["doc:posts"][i].isReported);
+                        var forumName = arr["_embedded"]["doc:posts"][i].forumName;
+                        var dt = new Date(epoch * 1000);
+                        var formattedDate = strPad(dt.getHours()) + ":" + strPad(dt.getMinutes()) + ":" + strPad(dt.getSeconds());
 
-                //Create HTML for date:
-                var spanDate = document.createElement("span");
-                spanDate.className = "df-date";
-                var spanDateText = document.createTextNode(formattedDate + " — ");
-                spanDate.appendChild(spanDateText);
+                        //Create HTML for date:
+                        var spanDate = document.createElement("span");
+                        spanDate.className = "df-date";
+                        var spanDateText = document.createTextNode(formattedDate + " — ");
+                        spanDate.appendChild(spanDateText);
 
-                //Create HTML for message body:
-                var aMessage = document.createElement("a");
-                aMessage.className = "df-content";
-                aMessage.href = "/d/p/" + threadID;
-                aMessage.target = "_blank";
-                var aMessageText = document.createTextNode(text);
-                aMessage.appendChild(aMessageText);
+                        //Create HTML for message body:
+                        var aMessage = document.createElement("a");
+                        aMessage.className = "df-content";
+                        aMessage.href = "/d/p/" + threadID;
+                        aMessage.target = "_blank";
+                        var aMessageText = document.createTextNode(text);
+                        aMessage.appendChild(aMessageText);
 
-                //Create HTML for user:
-                var aUser = document.createElement("a");
-                aUser.className = "df-user";
-                aUser.href = "/d/u/" + userId;
-                aUser.target = "_blank";
-                var aUserText = document.createTextNode(" — " + user);
-                aUser.appendChild(aUserText);
+                        //Create HTML for user:
+                        var aUser = document.createElement("a");
+                        aUser.className = "df-user";
+                        aUser.href = "/d/u/" + userId;
+                        aUser.target = "_blank";
+                        var aUserText = document.createTextNode(" — " + user);
+                        aUser.appendChild(aUserText);
 
-                //Create HTML for category:
-                var spanCategory = document.createElement("span");
-                spanCategory.className = "df-category";
-                var spanCategoryText = document.createTextNode(" in " + forumName);
-                spanCategory.appendChild(spanCategoryText);
+                        //Create HTML for category:
+                        var spanCategory = document.createElement("span");
+                        spanCategory.className = "df-category";
+                        var spanCategoryText = document.createTextNode(" in " + forumName);
+                        spanCategory.appendChild(spanCategoryText);
 
-                //Create block button
-                if (canBlock) {
-                    var aBlock = document.createElement("a");
-                    aBlock.className = "df-block";
-                    aBlock.href = "/wiki/Special:Block/" + user;
-                    aBlock.target = "_blank";
-                    var aBlockText = document.createTextNode(" (block)");
-                    aBlock.appendChild(aBlockText);
-                }
+                        //Create block button
+                        if (canBlock) {
+                            var aBlock = document.createElement("a");
+                            aBlock.className = "df-block";
+                            aBlock.href = "/wiki/Special:Block/" + user;
+                            aBlock.target = "_blank";
+                            var aBlockText = document.createTextNode(" (block)");
+                            aBlock.appendChild(aBlockText);
+                        }
 
-                //Put everything together
-                var par = document.createElement("p");
-                par.className = "df-entry";
-                if (isReported) {
-                    par.className += " df-reported"
-                }
-                par.appendChild(spanDate);
-                par.appendChild(aMessage);
-                par.appendChild(aUser);
-                par.appendChild(spanCategory);
-                if (canBlock) {
-                    par.appendChild(aBlock);
-                }
-                content.appendChild(par);
+                        //Put everything together
+                        var par = document.createElement("p");
+                        par.className = "df-entry";
+                        if (isReported) {
+                            par.className += " df-reported";
+                        }
+                        par.appendChild(spanDate);
+                        par.appendChild(aMessage);
+                        par.appendChild(aUser);
+                        par.appendChild(spanCategory);
+                        if (canBlock) {
+                            par.appendChild(aBlock);
+                        }
+                        content.appendChild(par);
+                    }
+                    break;
+                case 404:
+                    content.innerHTML = 'There are no posts.';
+                    break;
+                default:
+                    content.innerHTML = "Unable to load discussions: The service is down or is not enabled for this domain.";
             }
         }
     };
